500ddd669ae32adb0c1a987d09bf1a1f4df5e705
takeshi <exmcube@gmail.com>
2020-11-18 00:44:47 +0900

    なくなった修正点をコミット

--- mbed/Esc.cpp
+++ mbed/Esc.cpp
@@ -1,23 +1,13 @@
 #include "mbed.h"
 #include <ros.h>
+#include <Esc.h>
 #include <std_msgs/Float32.h>
 
-Esc::Esc(PinName pwm_pin)
+Esc::Esc(PinName pwm_pin) : PwmOut(pwm_pin)
 {
-  this->__motor(pwm_pin);
-  _motor.period_ms(PERIOD);
-  _motor.write(INITIAL_DUTY);
-  this->_is_debug = false;
+  this->period_ms(PERIOD);
+  this->write(INITIAL_DUTY);
 }
-
-// Esc::Esc(PinName pwm_pin, ros::NodeHandle nh)
-// {
-//   this->_motor(pwm_pin);
-//   _motor.period_ms(PERIOD);
-//   _motor.write(INITIAL_DUTY);
-//   this->_is_debug = true;
-//   nh.advertise(this->_debugger)
-// }
 
 Esc::~Esc()
 {
@@ -26,20 +16,16 @@ Esc::~Esc()
 
 void Esc::update(std_msgs::Float32 input)
 {
-   this->_write(_input.data);
-  //  if (this->is_debug)
-  //  {
-  //    this->_debugger.publish(&input);
-  //  }
+   _write(input.data);
 }
 
 void Esc::stop()
 {
-  this->_motor.write(0.0);
+  this->write(0.0);
 }
 
 void Esc::_write(float raw_duty)
 {
   float valid_duty = (MAX_DUTY - MIN_DUTY) * raw_duty + MIN_DUTY;
-  this->_motor.write(valid_duty);
+  this->write(valid_duty);
 }
--- mbed/HelloWorld.cpp
+++ mbed/HelloWorld.cpp
@@ -1,15 +1,10 @@
-/*
- * rosserial Publisher Example
- * Prints "hello world!"
- */
-
 #include "mbed.h"
 #include <ros.h>
 #include <BMI088.h>
-#include <geometry_msgs/Pose2D.h>
+#include <Esc.h>
 #include <std_msgs/String.h>
 #include <std_msgs/Float32.h>
-#include <std_msgs/UInt32.h>
+#include <std_msgs/Float32MultiArray.h>
 
 #define PERIOD  10
 #define MIN_DUTY 0.10  //Period 10ms
@@ -18,51 +13,49 @@
 // mbed variables
 DigitalOut led1 = LED1;
 DigitalOut led2 = LED2;
-PwmOut motor1(p21);
+
+Esc motor[4] = {p25, p24, p23, p22};
 
 // ros variables
 ros::NodeHandle nh;
-std_msgs::Float32 duty;
-ros::Publisher pub("now_duty", &duty);
-// std_msgs::UInt32 cnt;
-// ros::Publisher chatter("chatter", &cnt);
+std_msgs::Float32MultiArray duties;
+ros::Publisher duties_pub("now_duty", &duties);
 std_msgs::String echo;
 ros::Publisher debugger("debug_message", &echo); 
 
-void publish_string(string message)
+void publish_string(char* message)
 {
     echo.data = message;
     debugger.publish(&echo);
 }
 
-void init_duty()
-{
-    motor1.period_ms(PERIOD);
-    std_msgs::Float32 initial_duty;
-    initial_duty.data = 0.1;  //esc is initialized by 10% duty
-    write_duty(initial_duty);  
-}
+// void init_duty()
+// {
+//     motor1.period_ms(PERIOD);
+//     std_msgs::Float32 initial_duty;
+//     initial_duty.data = 0.1;  //esc is initialized by 10% duty
+//     write_duty(initial_duty);  
+// }
 
-void update_duty(const std_msgs::Float32& input_duty)
-{
-    write_duty(input_duty);
-    // chatter.publish(&cnt);
-    led1 = !led1;   
-    // cnt.data = cnt.data + 1;
-}
-ros::Subscriber<std_msgs::Float32> sub("input_duty", &update_duty);
+// void update_duty(const std_msgs::Float32& input_duty)
+// {
+//     write_duty(input_duty);
+//     // chatter.publish(&cnt);
+//     led1 = !led1;   
+//     // cnt.data = cnt.data + 1;
+// }
+// ros::Subscriber<std_msgs::Float32> sub("input_duty", &update_duty);
 
-void write_duty(std_msgs::Float32 input_duty)
-{
-    input_duty.data = (MAX_DUTY - MIN_DUTY) * input_duty.data + MIN_DUTY;
-    motor1.write(input_duty.data);
-    pub.publish(&input_duty);
-}
+// void write_duty(std_msgs::Float32 input_duty)
+// {
+//     input_duty.data = (MAX_DUTY - MIN_DUTY) * input_duty.data + MIN_DUTY;
+//     motor1.write(input_duty.data);
+//     pub.publish(&input_duty);
+// }
 
 void init_mbed()
 {
     publish_string("initialize mbed...");
-    init_duty();
     wait_ms(500);
 }
 
@@ -70,18 +63,16 @@ void init_ros()
 {
     publish_string("initialize ros...");
     nh.initNode();
-    nh.advertise(pub);
-    nh.subscribe(sub);
-    // nh.advertise(chatter);
-    // cnt.data = 0;
-    duty.data = 0.0;
+    nh.advertise(duties_pub);
+    nh.advertise(debugger);
+    // nh.subscribe(sub);
 }
 
 int main()
 {
     init_ros();
     init_mbed();
-    publish_string("start loop!")
+    publish_string("start loop!");
     while(1)
     {
         nh.spinOnce();

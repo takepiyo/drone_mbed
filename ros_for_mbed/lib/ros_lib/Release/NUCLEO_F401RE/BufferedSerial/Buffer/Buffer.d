1154ca76311a29c244c123de9323f309b57a99da
takeshi <exmcube@gmail.com>
2020-11-17 15:36:37 +0900

    ros_for_mbedのReleaseディレクトリ /mbed/LPC1768/ .elf .map　すべてを無視リスト追加

--- ros_for_mbed/lib/ros_lib/Release/NUCLEO_F401RE/BufferedSerial/Buffer/Buffer.d
+++
@@ -1,8 +0,0 @@
-/home/takeshi/ros/lib/ros_lib/Release/NUCLEO_F401RE/BufferedSerial/Buffer/Buffer.o: \
- /home/takeshi/ros/lib/ros_lib/BufferedSerial/Buffer/Buffer.cpp \
- /home/takeshi/gcc4mbed/src/mbed_config.h \
- /home/takeshi/ros/lib/ros_lib/BufferedSerial/Buffer/Buffer.h
-
-/home/takeshi/gcc4mbed/src/mbed_config.h:
-
-/home/takeshi/ros/lib/ros_lib/BufferedSerial/Buffer/Buffer.h:
--- ros_for_mbed/lib/ros_lib/Release/NUCLEO_F401RE/duration.d
+++
@@ -1,8 +0,0 @@
-/home/takeshi/ros/lib/ros_lib/Release/NUCLEO_F401RE/duration.o: \
- /home/takeshi/ros/lib/ros_lib/duration.cpp \
- /home/takeshi/gcc4mbed/src/mbed_config.h \
- /home/takeshi/ros/lib/ros_lib/ros/duration.h
-
-/home/takeshi/gcc4mbed/src/mbed_config.h:
-
-/home/takeshi/ros/lib/ros_lib/ros/duration.h:
